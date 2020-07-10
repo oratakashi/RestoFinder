5bc99eed70267af4e2d496c07bf2bc8a93c1f1ab
Oratakashi Nhamako <davidcreazy@gmail.com>
2020-07-10 13:32:48 +0700

    Create Config.kt

---
+++ app/src/main/java/com/oratakashi/resto/core/Config.kt
@@ -0,0 +1,15 @@
+package com.oratakashi.resto.core
+
+/**
+ * Change Key with your API Keys
+ *
+ * Get Api Keys on developers.zomato.com/api
+ *
+ * Untuk mendapatkan Api Keys kunjungi developers.zomato.com/api
+ */
+
+object Config {
+    const val Key = "{{ YOUR_API_KEY }}"
+    const val DummyImage =
+        "https://i.pinimg.com/originals/e2/62/6c/e2626c8f2581f6de2c9e325b67957a3b.jpg"
+}
